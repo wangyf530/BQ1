e2e9984f50639d3a0eac34dc84bf5ea50b83147f
yf wang <wangyf530@gmail.com>
2024-12-13 13:36:50 +0800

    在網站標題區顯示指定的標題圖片

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once "api/db.php" ?>
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
@@ -22,8 +23,9 @@
 	</div>
 	<iframe style="display:none;" name="back" id="back"></iframe>
 	<div id="main">
-		<a title="" href="./home_files/home.htm">
-			<div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div>
+		<a title="" href="index.php">
+			
+			<div class="ti" style="background:url('./upload/<?=$TITLE->find(['sh'=>1])['img'];?>'); background-size:cover;"></div>
 			<!--標題-->
 		</a>
 		<div id="ms">
